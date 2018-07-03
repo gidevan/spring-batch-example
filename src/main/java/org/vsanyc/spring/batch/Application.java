c189f382189957c7335fbe88c0e608c361037962
gidevan <vsanyc@gmail.com>
2018-07-03 15:10:34 +0300

    default initialization

---
+++ src/main/java/org/vsanyc/spring/batch/Application.java
@@ -0,0 +1,15 @@
+package org.vsanyc.spring.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by vano on 3.7.18.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
